3dd781bd84ab648c27e261d8e85046aeffa53b71
Jonathan Lam <jlam55555@gmail.com>
2021-06-17 21:14:41 -0400

    finished tutorial 7 (added transformation matrix)

--- src/main/java/shaders/StaticShader.java
+++ src/main/java/shaders/StaticShader.java
@@ -1,8 +1,12 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram {
     private static final String VERTEX_FILE = "src/main/resources/Vertex.shader";
     private static final String FRAGMENT_FILE = "src/main/resources/Fragment.shader";
+
+    private int transformationMatrixLocation;
 
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -12,5 +16,14 @@ public class StaticShader extends ShaderProgram {
     protected void bindAttributes() {
         super.bindAttribute(0, "position");
         super.bindAttribute(1, "textureCoords");
+    }
+
+    @Override
+    protected void getAllUniformLocations() {
+        transformationMatrixLocation = super.getUniformLocation("transformationMatrix");
+    }
+
+    public void loadTransformationMatrix(Matrix4f matrix) {
+        super.loadMatrix(transformationMatrixLocation, matrix);
     }
 }
